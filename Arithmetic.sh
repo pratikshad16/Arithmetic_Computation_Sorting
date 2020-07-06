41e2990261c2bc413a43e4e6bf8b6ed5ba865eac
pratikshad16 <dhagepratiksha16@gmail.com>
2020-07-06 22:48:47 +0530

    Added:Three inputs are added

--- Arithmetic.sh
+++ Arithmetic.sh
@@ -1,2 +1,6 @@
 #!/bin/bash -x
 echo "Welcome to Arithmetic Computation And Sorting Program"
+read -p "enter the  first value:" a
+read -p "enter the second value:" b
+read -p "enter the  third value:" c
+echo $a $b $c
