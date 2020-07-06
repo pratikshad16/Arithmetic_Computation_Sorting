53084c190490b94d48af80a8456262e4170e70a4
pratikshad16 <dhagepratiksha16@gmail.com>
2020-07-06 22:58:29 +0530

    Added:functionality for first computation

--- Arithmetic.sh
+++ Arithmetic.sh
@@ -1,6 +1,10 @@
 #!/bin/bash -x
+#UC1
 echo "Welcome to Arithmetic Computation And Sorting Program"
 read -p "enter the  first value:" a
 read -p "enter the second value:" b
 read -p "enter the  third value:" c
 echo $a $b $c
+#UC2
+result1=$(($a+$b*$c))
+echo $result1
